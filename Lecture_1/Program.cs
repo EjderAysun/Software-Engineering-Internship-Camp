6147192916d48253fe09b746aba8c5f5539f9167
Ejder Aysun <ejderaysunn@gmail.com>
2024-06-26 15:20:50 +0300

    Added main + .sln file + .csproj file

---
+++ Lecture_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Lecture_1.Camp_Intro;
+using Lecture_1.Homeworks;
+
+namespace Lecture_1 {
+    class Program {
+        public static void Main(string[] args) {
+            // Camp Intro
+            Fundamental.Intro();
+            ArrayAndForeach.ArrayExample();
+            Class.ClassExample();
+            ForLoop.ForLoopExample();
+
+            Console.WriteLine("HOMEWORKS");
+            Console.WriteLine("-----------------");
+            Homework.L1H1();
+            Homework.L1H2();
+            Homework.L1H3();
+            Homework.L1H4();
+            Homework.L1H5();
+            Homework.L1H6();
+        }
+    }
+}
